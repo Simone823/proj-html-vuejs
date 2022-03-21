402850fffafe15cb9708b4d1b005f848bffc2e10
Simone823 <simone.daglio823@outlook.it>
2022-03-21 17:11:52 +0100

    import component MyHeader

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,19 @@
 
   <!-- App vue -->
   <div id="app">
-    <h1>Web Design agency</h1>
+    <MyHeader/>
   </div>
 
 </template>
 
 <script>
+// Import MyHeader
+import MyHeader from "./components/MyHeader.vue"
 
 export default {
   name: 'App',
   components: {
-
+    MyHeader,
   }
 }
 </script>
