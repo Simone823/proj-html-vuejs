ee3956ce7889a18340e66d5d03487cb0e7522a95
Simone823 <simone.daglio823@outlook.it>
2022-03-22 01:57:19 +0100

    font-family quicksand, import Section_1

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,16 @@
   <div id="app">
     <MyHeader/>
     <NavMenu/>
+
+    <main>
+      <Section_1/>
+    </main>
+
   </div>
 
 </template>
+
+
 
 <script>
 // Import MyHeader
@@ -15,21 +22,27 @@ import MyHeader from "./components/MyHeader.vue";
 // Import NavMenu
 import NavMenu from "./components/NavMenu.vue";
 
+// Import Section_1
+import Section_1 from "./components/Section_1.vue";
+
 export default {
   name: 'App',
   components: {
     MyHeader,
     NavMenu,
+    Section_1,
   }
 }
 </script>
+
+
 
 <style lang="scss">
 // Import common scss
 @import '../src/assets/scss/common.scss';
 
 #app {
-  font-family: "Nunito", sans-serif;
+  font-family: "Quicksand", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
